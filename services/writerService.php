2a56a8454c0bda5ad0af2fae2d190b8ba8ee0635
Klenovszky_Áron_103 <aron.klenovszky@gmail.com>
2022-02-17 12:19:40 +0100

    Rewrite width new types

--- services/writerService.php
+++ services/writerService.php
@@ -15,7 +15,6 @@ class WriterService
 
     function getAll()
     {
-
         $query = "SELECT * FROM `writers`";
 
         $stmt = $this->conn->prepare($query);
@@ -27,7 +26,6 @@ class WriterService
     function getById($id) //should check idExists
     {
         $query = "SELECT * FROM `writers` WHERE `id` like ?";
-        $id = htmlspecialchars(strip_tags($id));
 
         $stmt = $this->conn->prepare($query);
         $stmt->bind_param("i", $id);
@@ -42,10 +40,8 @@ class WriterService
         $writer->firstName = htmlspecialchars(strip_tags($writer->firstName));
         $writer->lastName = htmlspecialchars(strip_tags($writer->lastName));
         $writer->bornIn = htmlspecialchars(strip_tags($writer->bornIn));
-        $writer->boenAt = htmlspecialchars(strip_tags($writer->bornAt));
-        $writer->died = htmlspecialchars(strip_tags($writer->died));
 
-        $stmt->bind_param("sssis", $writer->firstName, $writer->lastName, $writer->bornIn, $writer->bornAt, $writer->died);
+        $stmt->bind_param("sssii", $writer->firstName, $writer->lastName, $writer->bornIn, $writer->bornAt, $writer->died);
 
         return $stmt->execute();
     }
@@ -58,11 +54,8 @@ class WriterService
         $writer->firstName = htmlspecialchars(strip_tags($writer->firstName));
         $writer->lastName = htmlspecialchars(strip_tags($writer->lastName));
         $writer->bornIn = htmlspecialchars(strip_tags($writer->bornIn));
-        $writer->boenAt = htmlspecialchars(strip_tags($writer->bornAt));
-        $writer->died = htmlspecialchars(strip_tags($writer->died));
-        $writer->id = htmlspecialchars(strip_tags($writer->id));
 
-        $stmt->bind_param("sssisi", $writer->firstName, $writer->lastName, $writer->bornIn, $writer->bornAt, $writer->died, $writer->id);
+        $stmt->bind_param("sssiii", $writer->firstName, $writer->lastName, $writer->bornIn, $writer->bornAt, $writer->died, $writer->id);
 
         $stmt->execute();
         if ($stmt->affected_rows == 0) return false;
@@ -74,8 +67,6 @@ class WriterService
         $query = "DELETE FROM `writers` WHERE `id = ?";
         $stmt = $this->conn->prepare($query);
 
-        $id = htmlspecialchars(strip_tags($id));
-
         $stmt->bind_param("i", $id);
 
         return $stmt->execute();
@@ -84,7 +75,6 @@ class WriterService
     function idExists($id)
     {
         $query = "SELECT * FROM `writers` WHERE `id` like ?";
-        $id = htmlspecialchars(strip_tags($id));
 
         $stmt = $this->conn->prepare($query);
         $stmt->bind_param("i", $id);
