2ce9fdc03c95f0c50acd6661389c36e023a98351
Klenovszky_Áron_Home <aron.klenovszky@gmail.com>
2022-02-24 13:23:19 +0100

    Book create

---
+++ api/book/create.php
@@ -0,0 +1,48 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Request-Method: POST");
+    header("Content-Type: application/json; charset=UTF-8");
+
+    include '../../vendor/autoload.php';
+    use Firebase\JWT\JWT;
+    use Firebase\JWT\Key;
+
+    include_once("../../model/book.php");
+    include_once("../../services/bookService.php");
+    $book = new Book();
+    $bookService = new BookService();
+
+    if ($_SERVER["REQUEST_METHOD"] != "POST"){
+        http_response_code(405);
+        echo json_encode(array("message" => "Method not allowed."));
+        exit;
+    }
+
+    $data = json_decode(file_get_contents("php://input"));
+    if (!isset($data->title) || !isset($data->category) || 
+        !isset($data->published) || !isset($data->writerId) || !isset($data->jwt)) 
+    {
+        http_response_code(406);
+        echo json_encode(array("message" => "Bad JSON format."));
+        exit;
+    }
+
+    $book->title = $data->title;
+    $book->category = $data->category;
+    $book->published = $data->published;
+    $book->writerId = $data->writerId;
+    
+    $config = parse_ini_file('../../config.ini');
+    $key = $config['key'];
+    try {
+        $decoded = JWT::decode($data->jwt, new Key($key, 'HS256'));
+    } catch (\Throwable $th) {
+        http_response_code(401);
+        echo json_encode(array("message" => $th->getMessage()));
+        exit;
+    }
+    
+    $bookService->create($book);
+    http_response_code(200);
+    echo json_encode(array("message" => "Successfully create."));
+?>
